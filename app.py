6850cd555e11bed815d9f6c931e80ca4e85b0ffb
[dohunhyeong] <[hdhd4422@naver.com]>
2024-08-23 11:40:52 +0900

    first

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, request, jsonify, render_template
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
-import torch
-from peft import PeftModel
+import requests
 
 app = Flask(__name__)
 
-# 모델 로드
-base_model_name = "winglian/Llama-2-3b-hf"
-tokenizer = AutoTokenizer.from_pretrained(base_model_name)
-base_model = AutoModelForCausalLM.from_pretrained(base_model_name, torch_dtype=torch.float16)
+# Hugging Face Inference API 정보
+API_URL = "https://api-inference.huggingface.co/models/Nudge5/cdci-2-7b"
+HEADERS = {"Authorization": "Bearer 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"}
 
-# LoRA 모델 로드 및 적용
-lora_model_path = "models"  # 로컬 경로로 변경 필요
-model = PeftModel.from_pretrained(base_model, lora_model_path)
-
-# 파이프라인 설정
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_length=200, device=-1)  # GPU 사용
+def query_huggingface_api(prompt):
+    try:
+        response = requests.post(API_URL, headers=HEADERS, json={"inputs": prompt})
+        response.encoding = 'utf-8'
+        print("Response content:", response.content)
+        print("Response text:", response.text)
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        print("Error during API call:", e)
+        return {"error": "API request failed"}
 
 @app.route("/")
 def index():
@@ -23,19 +24,23 @@ def index():
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    # 요청에서 질문 가져오기
     data = request.json
     prompt = data.get("prompt", "")
     
     if not prompt:
         return jsonify({"error": "Prompt is required"}), 400
     
-    # 모델에 질문 입력
-    result = pipe(f'<s>[INST] {prompt}[/INST]')
+    result = query_huggingface_api(prompt)
     
-    # 결과 반환
-    return jsonify({"response": result[0]['generated_text']})
+    if "error" in result:
+        return jsonify({"error": result.get("error", "Unknown error occurred.")}), 500
+    
+    if isinstance(result, list) and len(result) > 0 and 'generated_text' in result[0]:
+        response_text = result[0]['generated_text']
+        answer = response_text.replace(prompt, "").strip()
+        return jsonify({"response": answer})
+    else:
+        return jsonify({"error": "Invalid response structure or no generated text."}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True,host="0.0.0.0", port=5000)
-    
+    app.run(debug=True, host="0.0.0.0", port=5000)
